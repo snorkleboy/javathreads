28b16cc7996b1266054f2d9098d78ce931fadb20
tim <timkharshan@hotmail.com>
2018-06-23 23:29:23 -0700

    took out threader class

--- src/tim/Main.java
+++ src/tim/Main.java
@@ -7,10 +7,8 @@ import tim.Task.tasks.QueueCheckTask;
 public class Main {
 
     public static void main(String[] args) {
-
         TaskQueue.queue.add(new DirectoryScanTask());
-        new Threader().run();
-        new QueueCheckTask().run();
+        Threader.spinUp();
     }
 }
 
