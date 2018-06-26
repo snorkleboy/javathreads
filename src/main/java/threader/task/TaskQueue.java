b286ae9dd41007f13a794465da3aa7ed30375f96
tim <timkharshan@hotmail.com>
2018-06-25 23:29:46 -0700

    back to sleep check ;{

--- src/main/java/threader/task/TaskQueue.java
+++ src/main/java/threader/task/TaskQueue.java
@@ -5,9 +5,10 @@ import threader.Log;
 import java.util.Queue;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.TimeUnit;
 
 public class TaskQueue {
-    public final static ConcurrentLinkedQueue<Task> queue = new ConcurrentLinkedQueue<Task>();
+    public final static ArrayBlockingQueue<Task> queue = new ArrayBlockingQueue<Task>(20);
     private static ConcurrentLinkedQueue<Thread> waitlist = new ConcurrentLinkedQueue<Thread>();
 //    private static ConcurrentLinkedQueue<Thread> threadlist = new ConcurrentLinkedQueue<Thread>();
 
@@ -24,25 +25,28 @@ public class TaskQueue {
             checkTasks();
         }
     }
-    public static Task poll(){
-        Task task =  queue.poll();
-        return task;
+    public static Task poll() throws InterruptedException{
+//        Task task =  queue.poll();
+//        return task;
+        return queue.poll(5l, TimeUnit.SECONDS);
     }
     public static Boolean hasTasks(){
         return queue.peek() != null;
     }
     public static void add(Task task){
         queue.add(task);
-        Thread thread = waitlist.poll() ;
-        if(thread != null){
-            synchronized (thread){
-                thread.notify();
-            }
-        }
+//        Thread thread = waitlist.poll();
+//        System.out.println("POLL THREADS   " + thread==null  );
+//        if(thread != null){
+//            synchronized (thread){
+//                thread.notify();
+//            }
+//        }
     }
     public static void checkTasks(){
         while(true){
             log.log(Thread.currentThread().getName(),Thread.currentThread().getName() + " poll queue");
+
             Task task = queue.poll();
             if (task != null){
                 System.out.println(Thread.currentThread().getName()+" FOUNT TASK  " + task.getClass());
@@ -54,21 +58,24 @@ public class TaskQueue {
                     e.printStackTrace();
                 }
             }else{
-                System.out.println(Thread.currentThread().getName() + " poll queue     waiting:");
-                try{
-                    Thread thread = Thread.currentThread();
-                    synchronized (thread){
-                        waitlist.add(Thread.currentThread());
-                        log.log(thread.getName(),Thread.currentThread().getName() + " wait indef");
-                        System.out.println(thread.getName()+" wait indef");
-                        while(true){
-                            thread.wait();
-                        }
-                    }
-                }catch (InterruptedException e){
-                    System.out.println(Thread.currentThread().getName()+" AWOKEN");
-                    checkTasks();
-                }
+                Task.sleep(1500);
+//                log.print();
+
+//                System.out.println(Thread.currentThread().getName() + " poll queue     waiting:");
+//                try{
+//                    Thread thread = Thread.currentThread();
+//                    synchronized (thread){
+//                        waitlist.add(Thread.currentThread());
+//                        log.log(thread.getName(),Thread.currentThread().getName() + " wait indef");
+//                        System.out.println(thread.getName()+" wait indef");
+//                        while(true){
+//                            thread.wait();
+//                        }
+//                    }
+//                }catch (InterruptedException e){
+//                    System.out.println(Thread.currentThread().getName()+" AWOKEN");
+//                    checkTasks();
+//                }
             }
         }
     }
